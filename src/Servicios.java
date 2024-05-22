cd73c5f60b317b1b1f02692ffeefd84d79c022bf
FFernandez3 <florenciafernandez0301@gmail.com>
2024-05-22 20:37:40 -0300

    started with backtracking

--- src/Servicios.java
+++ src/Servicios.java
@@ -14,6 +14,42 @@ import java.util.List;
 public class Servicios {
     private HashMap<String,Tarea> tareas;
     private HashMap<String, Procesador> procesadores;  //ver si lo vamos a usar
+    //parte 2
+    private int tiempoMaximo; //resultado final
+    private HashMap<Procesador,List<Tarea>> asignacion; //resultado final
+
+    public Servicios(){
+        asignacion=new HashMap<>();
+        this.tiempoMaximo=0;
+    }
+
+    public HashMap<Procesador,List<Tarea>> asignacionTareasEnProcesadores(int tiempoX){
+        int tiempoAux=0;
+        HashMap<Procesador,List<Tarea>> asignacionParcial=new HashMap<>();
+        backtracking(tiempoAux, tiempoX, asignacionParcial, "T1", "P1");
+        return asignacion;
+
+    }
+    private void backtracking(int tiempoAux, int tiempoX, HashMap<Procesador,List<Tarea>> asignacionParcial, String tareaActual, String procesadorActual ){
+       //casos de corte
+        si ya no hay tareas para asignar
+                corto recursion
+        if(!procesadores.get(procesadorActual).isRefrigerado() && tareas.get(tareaActual).getTpo_ejecucion()>tiempoX ){
+            return;
+        }
+        else if () {
+
+        }
+        //actualizar estado
+        if tiempoAux<tiempoMaximo
+                tiempoMaximo=tiempoAux
+                asignacion=asignacionParcial
+        //recursion
+        si quedan tareas para asignaar
+                recursion
+        //deshacer cambios estado
+    }
+
 
     /*
      * Expresar la complejidad temporal del constructor.
